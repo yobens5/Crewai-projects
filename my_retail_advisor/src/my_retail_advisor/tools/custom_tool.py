c0f8ce90c437bbea4515013faac2e4fbd5630278
yobens5 <yobens5@gmail.com>
2024-11-23 21:10:02 +0200

    v0.1

--- my_retail_advisor/src/my_retail_advisor/tools/custom_tool.py
+++ my_retail_advisor/src/my_retail_advisor/tools/custom_tool.py
@@ -1,15 +1,32 @@
 from crewai.tools import BaseTool
 from my_retail_advisor.tools.tool_helper import Helper
 
+
 class VisionTool(BaseTool):
     name: str = "Vision Tool"
     description: str = "Analyzes a default picture to collect visual data and caption."
+    
 
     def _run(self) -> str:
-        # Absolute path to the shelve.png image
+        # Absolute path to the shelve.png image 
         image_path = '/Users/yohanbensoussan/Projects/crewai/my_retail_advisor/shelve.jpg'
 
         # Simulating image-to-text conversion
         products_in_image = Helper.image2text(image_path)
         return products_in_image
 
+# # Activate it to include imag epath as argument
+# class ImagePromptSchema(BaseModel):
+#     """Input for Vision Tool."""
+#     image_path_url: str = "The image path or URL."
+
+# class VisionTool(BaseTool):
+#     name: str = "Vision Tool"
+#     description: str = "Analyzes a default picture to collect visual data and caption."
+#     # Activate it to include imag epath as argument
+#     args_schema: Type[BaseModel] = ImagePromptSchema
+
+#     def _run(self, client, image_path_url: str) -> str:
+#          # Simulating image-to-text conversion
+#         products_in_image = Helper.image2text(image_path)
+#         return products_in_image
